626b7104fed5d485926fad99da7c1c1cfd54d916
Yiğit KABAK <yigitkabak@tuta.io>
2026-01-22 23:44:14 +0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave: false,
   devServer: {
+    port: 8080,
     proxy: {
-      '^/api': {
-        target: 'http://127.0.0.1:3000',
+      '/api': {
+        target: 'https://uptimeapi.synapic.com.tr',
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        secure: false
+      }
+    }
+  }
 })
